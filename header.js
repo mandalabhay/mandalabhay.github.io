48f6751cd2316fcae96d9439ac9b2c3e47ce9948
mandalabhay <abhaymandal@gmail.com>
2018-08-22 09:48:31 +0530

    Header include created

---
+++ header.js
@@ -0,0 +1,44 @@
+document.write(`<nav class="navbar navbar-expand-lg text-white mb-3 py-3">
+  <a class="navbar-brand" href="index.html">Portfolio</a>
+  <button class="navbar-toggler hidden-sm-up pull-right p-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"><i class="fas fa-bars size24 greyDark"></i></span>
+  </button>
+
+  <div class="navbar-collapse collapse show" id="navbarSupportedContent" style="">
+    <ul class="navbar-nav ml-auto">
+    	<li>
+            <a class="nav-link" href="index.html">
+              Design
+            </a>
+            
+        </li>
+        
+        <li class="dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Code
+            </a>
+            <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+               <li class="dropdown-item"><a href="javascript.html">Java Script</a></li>
+               <li class="dropdown-item"><a href="css/html">CSS</a></li>
+              </ul>
+        </li>
+     
+        <li>
+            <a class="nav-link" href="justme.html">
+              Just Me !
+            </a>
+        </li>
+      <li>
+            <a class="nav-link" href="Resume.html">
+             Resume
+            </a>
+        </li>
+
+      
+      
+      
+    </ul>
+     
+  </div>
+ 
+</nav>`)
