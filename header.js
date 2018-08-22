9984caa6cff11633b5cad001f5c479605104316e
mandalabhay <abhaymandal@gmail.com>
2018-08-22 09:51:15 +0530

    Update header.js

--- header.js
+++ header.js
@@ -1,4 +1,5 @@
-document.write(`<nav class="navbar navbar-expand-lg text-white mb-3 py-3">
+document.write(`<header class="sticky-top">
+<nav class="navbar navbar-expand-lg text-white mb-3 py-3">
   <a class="navbar-brand" href="index.html">Portfolio</a>
   <button class="navbar-toggler hidden-sm-up pull-right p-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"><i class="fas fa-bars size24 greyDark"></i></span>
@@ -7,12 +8,10 @@ document.write(`<nav class="navbar navbar-expand-lg text-white mb-3 py-3">
   <div class="navbar-collapse collapse show" id="navbarSupportedContent" style="">
     <ul class="navbar-nav ml-auto">
     	<li>
-            <a class="nav-link" href="index.html">
+           <a class="nav-link" href="index.html">
               Design
             </a>
-            
         </li>
-        
         <li class="dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Code
@@ -22,23 +21,17 @@ document.write(`<nav class="navbar navbar-expand-lg text-white mb-3 py-3">
                <li class="dropdown-item"><a href="css/html">CSS</a></li>
               </ul>
         </li>
-     
         <li>
             <a class="nav-link" href="justme.html">
               Just Me !
             </a>
         </li>
       <li>
-            <a class="nav-link" href="Resume.html">
+           <a class="nav-link" href="Resume.html">
              Resume
             </a>
         </li>
-
-      
-      
-      
-    </ul>
-     
+   </ul>
   </div>
- 
-</nav>`)
+</nav>
+</header>`)
